cdad6e6b21d6a03e299ea23f166f6483e1a2d48a
lilykoff <koffmanlily@gmail.com>
2025-04-29 14:57:30 -0400

    fix readme

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Example sample dataset
+#'
+#' A dataset included in accelPrint to illustrate usage.
+#'
+#' @format A data frame with N rows and M columns:
+#' \describe{
+#'   \item{x}{Numeric. X-axis value.}
+#'   \item{y}{Numeric. Y-axis value.}
+#'   \item{z}{Numeric. Z-axis value.}
+#'   \item{time}{POSIXct. Time stamp.}
+#' }
+#' @source Simulated data or citation if applicable
+"sample_df"
